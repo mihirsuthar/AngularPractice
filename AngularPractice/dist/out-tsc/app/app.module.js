8d51e2491997e121f25e7a9192acda2df1b81c72
Mihir <Mihir@DESKTOP-ICV14TL>
2019-03-24 23:10:26 +0530

    Add project files.

---
+++ AngularPractice/dist/out-tsc/app/app.module.js
@@ -0,0 +1,27 @@
+import * as tslib_1 from "tslib";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = tslib_1.__decorate([
+        NgModule({
+            declarations: [
+                AppComponent,
+                NavBarComponent
+            ],
+            imports: [
+                BrowserModule,
+                AppRoutingModule
+            ],
+            providers: [],
+            bootstrap: [AppComponent]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+export { AppModule };
+//# sourceMappingURL=app.module.js.map
---
+++ AngularPractice/dist/out-tsc/app/test/test.component.js
@@ -0,0 +1,19 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+var TestComponent = /** @class */ (function () {
+    function TestComponent() {
+    }
+    TestComponent.prototype.ngOnInit = function () {
+    };
+    TestComponent = tslib_1.__decorate([
+        Component({
+            selector: 'app-test',
+            templateUrl: './test.component.html',
+            styleUrls: ['./test.component.css']
+        }),
+        tslib_1.__metadata("design:paramtypes", [])
+    ], TestComponent);
+    return TestComponent;
+}());
+export { TestComponent };
+//# sourceMappingURL=test.component.js.map
